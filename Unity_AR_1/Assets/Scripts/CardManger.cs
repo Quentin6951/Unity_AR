df620a47dee2f7637fc07d33fdc3376533979879
Quentin6951 <105775617+Quentin6951@users.noreply.github.com>
2022-07-27 16:35:20 +0800

    AR_Game

--- Unity_AR_1/Assets/Scripts/CardManger.cs
+++ Unity_AR_1/Assets/Scripts/CardManger.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
 namespace Quentin.AR.Vuforia
 {
     /// <summary>
@@ -10,22 +14,56 @@ namespace Quentin.AR.Vuforia
     {
         [SerializeField, Header("KID圖片")]
         private DefaultObserverEventHandler observerKID;
+        [SerializeField, Header("小騎士")]
+        private Animator aniknight;
+        [SerializeField, Header("攻擊")]
+        private Button btnAttack;
+        [SerializeField, Header("虛擬按鈕跳躍")]
+        private VirtualButtonBehaviour vbbJump;
 
+        private string parVictory = "觸發勝利";
+        private AudioSource audBGM;
         private void Awake()
         {
             observerKID.OnTargetFound.AddListener(cardFound);
-            observerKID.OnTargetFound.AddListener(cardLost);
+            observerKID.OnTargetLost.AddListener(cardLost);
+            
+            btnAttack.onClick.AddListener(Attack);
+
+            vbbJump.RegisterOnButtonPressed(OnJumpPressed);
+
+            audBGM = GameObject.Find("BGM").GetComponent<AudioSource>();
         }
-        
+
+        private void OnJumpPressed(VirtualButtonBehaviour obj)
+        {
+            print("<color=#3366dd>跳躍!!!</color>");
+        }
+
+        /// <summary>
+        ///  圖片辨識成功
+        /// </summary>
 
         private void cardFound()
         {
             print("<color=yellow>找到卡片</color>");
+            aniknight.SetTrigger(parVictory);
+            audBGM.Play();
         }
+
+        /// <summary>
+        /// 圖片辨識失敗
+        /// </summary>
 
         private void cardLost()
         {
             print("<color=red>卡片辨識失敗</color>");
+            audBGM.Stop();
+        }
+
+        private void Attack()
+        {
+            print("<color=#9955aa>攻擊!!!</color>");
         }
 
 
